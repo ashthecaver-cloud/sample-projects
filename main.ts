0690c724de3c36f26edd1756b85566e8454b6de6
John Walter <ashthecaver@gmail.com>
2025-09-01 18:59:15 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.showIcon(IconNames.Butterfly)
+basic.showString("Hello!")
+basic.showArrow(ArrowNames.North)
+music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.UntilDone)
